e20afe136ced787591f07dccc62ed5667443e7b0
Gareth Callanan <gcallanan@gmail.com>
2024-06-17 11:48:04 +0200

    Buffer size is now parametric.

--- matrix_multiplication/2_systolic_array/compile_software.sh
+++ matrix_multiplication/2_systolic_array/compile_software.sh
@@ -3,6 +3,7 @@ set -e
 rm -rf myproject/build
 
 streamblocks multicore --set experimental-network-elaboration=on --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
+#tychoc --set experimental-network-elaboration=on --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
 
 mkdir -p  myproject/build/
 cd myproject/build/
