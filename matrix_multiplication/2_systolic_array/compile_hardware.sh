3b1baa846ed0009ded63e2bee4c3008e9e03a3b0
Gareth Callanan <gcallanan@gmail.com>
2024-06-18 15:38:24 +0200

    Script to generate hardware generates a bit faster now

--- matrix_multiplication/2_systolic_array/compile_hardware.sh
+++ matrix_multiplication/2_systolic_array/compile_hardware.sh
@@ -4,7 +4,7 @@ current_date_time="`date +%Y-%m-%dT%H:%M:%S`";
 
 rm -rf myproject/build
 
-streamblocks vivado-hls --set experimental-network-elaboration=on --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
+streamblocks vivado-hls --set experimental-network-elaboration=on --set reduction-algorithm=ordered-condition-checking --set default-controller=bc --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
 #tychoc --set experimental-network-elaboration=on --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
 bash myproject/scripts/generateSimpleHDLTestbenches_vivado2023.sh
 
