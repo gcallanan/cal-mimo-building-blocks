5ab3dafa216662307a7fc57600b9befb2638d5e7
Gareth Callanan <gcallanan@gmail.com>
2024-07-15 11:17:08 +0200

    Hardware script in systolic array skips AM generation by default.

--- matrix_multiplication/2_systolic_array/compile_hardware.sh
+++ matrix_multiplication/2_systolic_array/compile_hardware.sh
@@ -6,7 +6,8 @@ current_date_time="`date +%Y-%m-%dT%H:%M:%S`";
 
 rm -rf myproject/build
 
-streamblocks vivado-hls --set experimental-network-elaboration=on --set reduction-algorithm=ordered-condition-checking --set default-controller=bc --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
+streamblocks vivado-hls --set experimental-network-elaboration=on --set bypass-AM-generation=on --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
+#streamblocks vivado-hls --set experimental-network-elaboration=on --set reduction-algorithm=ordered-condition-checking --set default-controller=bc --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
 #tychoc --set experimental-network-elaboration=on --source-path matrix_mult_systolic.cal --target-path myproject mat_mul.Top
 bash myproject/scripts/generateSimpleHDLTestbenches_vivado2023.sh
 
